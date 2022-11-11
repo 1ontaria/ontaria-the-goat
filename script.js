a0e39fb8f260862ae52f4cd94b5af95898403b6f
Ontaria Woods <ontaria.woods@gmail.com>
2022-11-11 05:38:47 -0500

    added JS event listener, played with DOM

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const div = document.querySelector("div");
+const h1 = document.createElement("h1");
+div.appendChild(h1);
+h1.textContent = "About Me";
 
-document.querySelector("body").appendChild(h2);
+const body = document.querySelector("body");
+const h3 = document.createElement("h3");
+body.appendChild(h3);
+h3.textContent =
+  "Hi, my name is Ontaria Woods, and I am a Software Engineer in the making!!";
+
+document.getElementById("selfie").addEventListener("click", () => {
+  alert("Submitted!");
+});
+
+/*const p = document.createElement("p");
+body.append(p);
+p.textContent = <img src="Ontaria.png" />;*/
